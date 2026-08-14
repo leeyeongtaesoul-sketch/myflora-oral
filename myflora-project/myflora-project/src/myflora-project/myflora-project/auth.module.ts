f98aff56396ccfdfd9006f247608f55ffbe1bcec
leeyeongtaesoul-sketch <leeyeongtae.soul@gmail.com>
2026-08-14 18:00:32 +0800

    Update auth.module.ts

--- myflora-project/myflora-project/src/myflora-project/myflora-project/auth.module.ts
+++ myflora-project/myflora-project/src/myflora-project/myflora-project/auth.module.ts
@@ -1,1 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: 'myflora-secret-key',
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
