2aa2649cad5424be08eb2acb147ef9fd770fce88
leeyeongtaesoul-sketch <leeyeongtae.soul@gmail.com>
2026-08-14 22:11:09 +0800

    Update app.module.ts

--- myflora-project/src/src/app.module.ts
+++ myflora-project/src/src/app.module.ts
@@ -1,8 +1,2 @@
-import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ClinicModule } from './modules/clinic/clinic.module';
-
-@Module({
-  imports: [AuthModule, ClinicModule],
-})
-export class AppModule {}
