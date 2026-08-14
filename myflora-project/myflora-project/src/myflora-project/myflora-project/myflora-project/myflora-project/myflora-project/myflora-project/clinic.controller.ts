23b536659a7384586fb5277a37cb42397898c6f3
leeyeongtaesoul-sketch <leeyeongtae.soul@gmail.com>
2026-08-14 18:06:26 +0800

    Update clinic.controller.ts

--- myflora-project/myflora-project/src/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/clinic.controller.ts
+++ myflora-project/myflora-project/src/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/clinic.controller.ts
@@ -1,1 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
 
+@Controller('clinic')
+export class ClinicController {
+  @Get('hello')
+  hello() {
+    return { message: 'MyFlora Oral Clinic API is running!' };
+  }
+}
