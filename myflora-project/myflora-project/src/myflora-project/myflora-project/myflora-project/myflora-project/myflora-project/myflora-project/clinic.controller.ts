b1d2a0f6140281bb855bf40f70015b3aa34747a3
leeyeongtaesoul-sketch <leeyeongtae.soul@gmail.com>
2026-08-14 18:36:16 +0800

    Delete myflora-project directory

--- myflora-project/myflora-project/src/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/myflora-project/clinic.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('clinic')
-export class ClinicController {
-  @Get('hello')
-  hello() {
-    return { message: 'MyFlora Oral Clinic API is running!' };
-  }
-}
